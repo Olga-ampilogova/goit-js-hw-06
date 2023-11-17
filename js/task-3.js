48907fad7ebb01e21a4e71ec533ca025a4ae1dc8
Olga-ampilogova <141365234+Olga-ampilogova@users.noreply.github.com>
2023-11-17 12:43:32 +0100

    Update hometask

--- js/task-3.js
+++ js/task-3.js
@@ -1,44 +1,30 @@
-const sortByDescendingFriendCount = (users) => users
-  .toSorted((firstUser, secondUser) => (secondUser.friends.length - firstUser.friends.length));
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value
+  };
 
-
-
-  console.log(
-  sortByDescendingFriendCount([
-    {
-      name: "Moore Hensley",
-      friends: ["Sharron Pace"],
-      gender: "male"
-    },
-    {
-      name: "Sharlene Bush",
-      friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female"
-    },
-    {
-      name: "Ross Vazquez",
-      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male"
-    },
-    {
-      name: "Elma Head",
-      friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female"
-    },
-    {
-      name: "Carey Barr",
-      friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male"
-    },
-    {
-      name: "Blackburn Dotson",
-      friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male"
-    },
-    {
-      name: "Sheree Anthony",
-      friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female"
-    }
-  ])
-);
+  padStart (str)  {
+    let commLen = str.length + this.#value.length;
+    this.#value = this.#value.padStart(commLen, str);
+  }
+  padEnd (str) {
+    let commLen = str.length + this.#value.length;
+    this.#value = this.#value.padEnd(commLen, str);
+  }
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+ }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue());
+ builder.padStart("^");
+console.log(builder.getValue());
+builder.padEnd("^");
+console.log(builder.getValue());
+builder.padBoth("=");
+console.log(builder.getValue())
